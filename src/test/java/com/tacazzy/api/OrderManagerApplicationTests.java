15f3b1a3afd295f36afa4916d933d344542ffbce
Pablo <pabloh.anselmo@gmail.com>
2022-05-31 16:13:19 -0300

    Projeto Criado

---
+++ src/test/java/com/tacazzy/api/OrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacazzy.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
