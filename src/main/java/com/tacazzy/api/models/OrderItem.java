e7553fc9dd6e7e58ff88356fc38e62a7e9df1e0b
Pablo <pabloh.anselmo@gmail.com>
2022-06-07 14:39:53 -0300

    Entidade OrderItem, Relacionamento Many-To-Many com atributos extras part1

---
+++ src/main/java/com/tacazzy/api/models/OrderItem.java
@@ -0,0 +1,69 @@
+package com.tacazzy.api.models;
+
+import com.tacazzy.api.models.pk.OrderItemPk;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "TB_ORDER_ITEM")
+public class OrderItem implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 8613687247797688516L;
+
+    @EmbeddedId
+    private OrderItemPk id;
+
+    @Column
+    private Integer quantity;
+
+    @Column
+    private Double price;
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        this.id.setOrder(order);
+        this.id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/tacazzy/api/models/pk/OrderItemPk.java
@@ -0,0 +1,46 @@
+package com.tacazzy.api.models.pk;
+
+import com.tacazzy.api.models.Order;
+import com.tacazzy.api.models.Product;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Embeddable
+public class OrderItemPk implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 3452353509703023925L;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPk that = (OrderItemPk) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
