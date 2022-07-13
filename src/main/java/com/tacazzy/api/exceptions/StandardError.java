3e2b5c480162aafa0c7e7d0dd6e761f94878193b
Pablo <pabloh.anselmo@gmail.com>
2022-07-13 19:34:00 -0300

    Formatando data Com JsonFormat

--- src/main/java/com/tacazzy/api/exceptions/StandardError.java
+++ src/main/java/com/tacazzy/api/exceptions/StandardError.java
@@ -1,5 +1,6 @@
 package com.tacazzy.api.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,10 +17,11 @@ public class StandardError implements Serializable {
     @Serial
     private static final long serialVersionUID = -438244279989467403L;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy HH:mm:ss", timezone = "GMT")
     private Instant timestamp;
+
     private Integer status;
     private String error;
     private String message;
     private String path;
-
 }
